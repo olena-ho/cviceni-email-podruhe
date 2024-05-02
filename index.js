d0b0cbb72da535ae1d6cb89c7283f2831eea7c6f
Olena Horovenko <elena.gorovenko@gmail.com>
2024-05-02 21:52:21 +0200

    done

--- index.js
+++ index.js
@@ -1,14 +1,29 @@
 const goodbye = (name) => {
-  return 'S pozdravem ' + name;
+  return 'S pozdravem, ' + name;
+};
+
+const formalGoodbye = (name) => {
+  return `S uctivou poklonou, <br> ${name}`;
+};
+const rudeGoodbye = (name) => {
+  return 'Měj se, ' + name;
 };
 
 const fillSubject = (subject) => {
   document.querySelector('.email__subject').textContent = subject;
 };
 
-const fillBody = (body, name) => {
+const fillBody = (body, name, goodbyeFunction) => {
   const bodyElm = document.querySelector('.email__body');
   bodyElm.innerHTML = body;
   const closingElm = document.querySelector('.email__closing');
-  closingElm.textContent = goodbye(name);
+  closingElm.innerHTML = goodbyeFunction(name);
 };
+
+fillSubject('Obchodní sdělení');
+fillBody(
+  'Kupte mycí prostředek, který vám vytře zrak, se slevou 50 %.',
+  'Jan Čistý',
+  formalGoodbye
+);
+
